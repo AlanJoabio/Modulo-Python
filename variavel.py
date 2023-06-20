d8480b7f3f374ebc684ecc885112a59ea005613c
LanJob <alanjmsq@gmail.com>
2023-06-19 21:40:53 -0300

    Ajust word & Var

---
+++ variavel.py
@@ -0,0 +1,13 @@
+"""Para Comentarios"""
+
+"""
+Exemplos de Variavel
+nomeclatura erradas
+"""
+#Exemplos  de variavel com nomeclaturas erradas
+
+77nome = "Diego";
+
+ma$ = "mais";
+
+class = "Python";

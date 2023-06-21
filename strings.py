2a2be3cc2687a3abbcaf637764385d17e0d39c68
LanJob <alanjmsq@gmail.com>
2023-06-21 20:33:53 -0300

    Add auladeclaracaoString

--- strings.py
+++ strings.py
@@ -9,3 +9,15 @@ sobrenome = "Joabio";
 
 print (nome, sobrenome);
 """
+
+frase = "Olá Mundo!!"
+#Declarando uma string
+print("String original");
+print(frase);
+print();
+
+
+#Acessando caracteres individuais
+print ("Acessando caracteres individuais: ");
+print ("Primeiro caractere: ", frase[0]);
+print ("Ultimo caracter: ", frase[-1]);

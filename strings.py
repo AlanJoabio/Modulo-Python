7ffc8c872e1e15701d8a85f4a8e4d23f5e96d7ea
LanJob <alanjmsq@gmail.com>
2023-06-21 22:57:26 -0300

    Add a aula dia 21

--- strings.py
+++ strings.py
@@ -21,3 +21,22 @@ print();
 print ("Acessando caracteres individuais: ");
 print ("Primeiro caractere: ", frase[0]);
 print ("Ultimo caracter: ", frase[-1]);
+
+#Utilizando o input para pegar um nome informado pelo usuário
+nome = input("Nome: ");
+print (nome);
+print();
+
+#Inserindo as variáveis no print com f
+sobreNome = input("Sobrenome: ");
+print(f"Bem-vindo, {nome} {sobreNome}");
+print("Bem-vindo, {} {}".format(nome, sobreNome));
+
+#Concatenando strings
+print("Concatenação de Strings: ");
+outroFrase = "Bem-vindo";
+fraseCompleta = frase + ' ' + outroFrase;
+print(fraseCompleta);
+print()
+
+

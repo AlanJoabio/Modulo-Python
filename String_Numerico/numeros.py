0a14ca5c7b58037bb8de40c5f89f8cd48cdf2ccc
LanJob <alanjmsq@gmail.com>
2023-06-23 15:53:25 -0300

    Add pratica matematiica e numeros

---
+++ String_Numerico/numeros.py
@@ -0,0 +1,20 @@
+#Praticando da aula sobre o tipo de dado numericos
+
+#Operação matemática básicas
+num1 = 10;
+num2 = 5;
+
+soma            = num1 + num2;
+subtracao       = num1 - num2;
+multiplicacao   = num1 * num2;
+divisao         = num1 / num;
+resto_da_divisa = num1 % num2; #Restante da divisão, ou seja, quantos restam para completar uma divisão inteira.
+potencia        = num1 ** num2;
+
+print("Operação matemática básicas: ");
+print("Soma: ",soma);
+print("Subtração: ", subtracao);
+print("Multiplicação: ", multiplicacao);
+print("Divisão: ", divisao);
+print("Resto: ",resto_da_divisa);
+print("Potência: ", potencia);

9dfb5cc75e3b295f78e06c6118d7b8a6508b19de
LanJob <alanjmsq@gmail.com>
2023-06-29 20:40:15 -0300

    add aula tuplas

--- Tuplas/tuplas.py
+++ Tuplas/tuplas.py
@@ -2,7 +2,7 @@ tupla1 = (1,2,3,4,5);
 tupla2 = ("a","b","c");
 tupla3 = (1, "Hello", True);
 tupla4 = 1,2,3,4;
-tupla5 = ["a","b","c"]
+#tupla5 = ["a","b","c"]
 print(tupla1)
 print(tupla2)
 
@@ -17,5 +17,24 @@ print(tupla2[2]);
 print(tupla2[0]);
 
 #Conceito principal de tuplas; "Imutável!!"
-tupla5 [1] = "d";
-print(tupla5 [1]);
+#tupla5 [1] = "d";
+#print(tupla5 [1]);
+
+#Fatiamento de itens na tupla
+print(tupla1[1:4]);
+
+#Concatenando Tuplas
+tupla5 = 1,2,3;
+tupla6 = 4,5,6;
+tupla7 = tupla5 + tupla6; #concatena as duas tuplas
+print(tupla7);
+
+#Criando variaveis novas com os calores de uma tupla
+a, b, c = tupla6;
+a = tupla6[0];
+
+print()
+print("Valores das variáveis: ")
+print(a);
+print(b);
+print(c);

e294cc96a062c244303d8814b52109d626b7d87e
LanJob <alanjmsq@gmail.com>
2023-07-06 19:50:52 -0300

    add dicio

--- Dicionario/dicionario.py
+++ Dicionario/dicionario.py
@@ -1,16 +1,69 @@
-meuDicionario = {
-    'nome' : 'Alan',
-    'sobrenome' : 'Joabio',
-    'anos' : 33
-}
+# meuDicionario = {
+#     'nome' : 'Alan',
+#     'sobrenome' : 'Joabio',
+#     'anos' : 33
+# }
 
-print(meuDicionario);
+# print(meuDicionario);
+
+# frutaDicionario = {
+#     'maçã'   : 3,
+#     'banana' : 6,
+#     'uva'    : 8,
+# };
+
+# print("Significado encontrado no dicionário: ", frutaDicionario['maçã']);
+# print();
+
+# frutaDicionario["maçã"] = 5;
+# frutaDicionario["laranja"] = 10;
+
+# print("Nova quantidade de maça: ", frutaDicionario["maçã"]);
+# print();
+# print(frutaDicionario)
+
+
+# animaisDicionario = {};
+# animaisDicionario["cachorro"] = "Melissa";
+# print(animaisDicionario);
+
+
+# aluno = {
+#     'nome' : 'Alan Joanio' ,
+#     'idade': 33 ,
+#     'hobbies' : ['jogar csgo','esportes']
+# }
+
+# print(aluno);
+
+frutaDicionario = {
+    'maça'   : 3,
+    'banana' : 6,
+    'uva'    : 8,
+    'laranja': 10
+};
+
+print();
+print("Quantidade de maçãs: ", frutaDicionario.get("maçã"));
+print("Quantidade de bananas: ", frutaDicionario.get("banana"));
+
+print("Quantidade de morangos: ", frutaDicionario.get("morangos","Não foi encontrado a definição de morangos"));
+print();
+
+elementoRemovido = frutaDicionario.pop("laranja");
+
+print(elementoRemovido);
+print();
+print("Dicionário atualizado: ", frutaDicionario)
 
 frutaDicionario = {
     'maçã'   : 3,
     'banana' : 6,
     'uva'    : 8,
+    'laranja': 10
 };
 
-print("Significado encontrado no dicionário: ", frutaDicionario['maçã']);
 print();
+print("Chaves encontradas no dicionário: ", frutaDicionario.keys());
+print("Valores encontrados no dicionário: ",frutaDicionario.values());
+print(frutaDicionario.items());

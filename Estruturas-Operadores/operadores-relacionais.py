16933d8a53c8a3ed492bee9628fa90de3c86e101
LanJob <alanjmsq@gmail.com>
2023-07-08 15:38:31 -0300

    Add pasta do operadores relacionais

---
+++ Estruturas-Operadores/operadores-relacionais.py
@@ -0,0 +1,25 @@
+'''
+Operadores relacionais
+'''
+
+x = 5;
+y = 7;
+
+# maior que
+print(x > y); # False, pois x é menor ou igual a y.
+print();
+# maior ou igual à (>=)
+print (x >= y); # False, pois x é maior ou igual a y.
+print();
+# menor que (<)
+print(x < y); # True, pois x é menor do que y.
+print();
+# menor ou igual à (<=) 
+print(x <= y); # False
+print();
+#igualdade
+print(x == y); #False
+print();
+#diferença entre os valores (!=)
+print(x!= y); #True
+

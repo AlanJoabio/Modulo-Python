92eecf5739712bc62cb4fe2132e97ba61fb8be12
LanJob <alanjmsq@gmail.com>
2023-07-09 11:09:36 -0300

    add exercicio 03

--- Exercicios/exercicios03.py
+++ Exercicios/exercicios03.py
@@ -56,4 +56,42 @@ numero03 = int(input("Digite o terceiro número: "));
 if numero01 > 0 or numero02 > 0 or numero03 > 0:
     print("Pelo menos um dos número é positivo.")
 else:
-    print("Nenhum dos números é positivos>")
+    print("Nenhum dos números é positivos>");
+
+#7
+letra = input("Digite uma letra: ");
+
+if letra.lower() in "aeiou":
+    print("A letra é uma vogal.");
+else:
+    print("A letra não é uma vogal.");
+
+#8
+numero = int(input("Digite um número inteiro: "))
+
+if numero > 0:
+    print("O número é positivo.");
+elif numero < 0:
+    print("O número é negativo.");
+else:
+    print("O número é zero.");
+
+#9
+numero1 = float(input("Digite o primeiro número: "));
+numero2 = float(input("Digite o segundo número: "));
+numero3 = float(input("Digite o terceiro número: "));
+
+if numero1 < numero2 < numero3:
+    print("Os números estão em ordem crescente.");
+else:
+    print("Os números não estão em ordem crescente.");
+
+#10
+numero = int(input("Digite um número inteiro: "))
+
+if numero % 3 == 0 and numero % 5 == 0:
+    print("O número é um múltiplo de 3 e 5 ao mesmo tempo.");
+else:
+    print("O número não é um múltiplo de 3 e 5 ao mesmo tempo.");
+
+

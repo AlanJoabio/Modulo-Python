1bec9ff88c5ec5329b188df573a12b317c5b054a
LanJob <alanjmsq@gmail.com>
2023-07-17 21:26:17 -0300

    Add ex aula poo

---
+++ POO/main.py
@@ -0,0 +1,16 @@
+from conta_bancaria_final import Conta
+
+conta = input("Informe o número da conta: ");
+titular = input("Informe o titular da conta: ");
+saldo = float(input("Informe o saldo da conta: "));
+
+#Criando uma instância da classe
+minhaConta = Conta(conta, titular, saldo);
+
+depositar = float(input("Informe o valor para depósito: "));
+sacar = float(input("Informe o valor de saque: "));
+
+minhaConta.depositar(depositar);
+minhaConta.sacar(sacar);
+
+minhaConta.extrato();
